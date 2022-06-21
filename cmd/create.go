50f659d886b041eb19eaeedfdf61a13a6dfb7311
Mariano Cano <mariano.cano@gmail.com>
2022-06-20 17:28:54 -0700

    Add command to create a new key

---
+++ cmd/create.go
@@ -0,0 +1,184 @@
+/*
+Copyright © 2022 Smallstep Labs, Inc.
+
+*/
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/smallstep/step-kms-plugin/internal/flagutil"
+	"github.com/spf13/cobra"
+	"go.step.sm/crypto/kms"
+	"go.step.sm/crypto/kms/apiv1"
+	"go.step.sm/crypto/pemutil"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create <uri>",
+	Short: "generates a key in the KMS",
+	Long: `TODO: A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			cmd.SilenceErrors = true
+			return errors.New("usage")
+		}
+
+		flags := cmd.Flags()
+
+		kty := flagutil.MustString(flags, "kty")
+		crv := flagutil.MustString(flags, "crv")
+		size := flagutil.MustInt(flags, "size")
+		hash := flagutil.MustString(flags, "hash")
+		pss := flagutil.MustBool(flags, "pss")
+		extractable := flagutil.MustBool(flags, "extractable")
+		pl := flagutil.MustString(flags, "protection-level")
+
+		signatureAlgorithm := getSignatureAlgorithm(kty, crv, hash, pss)
+		if signatureAlgorithm == apiv1.UnspecifiedSignAlgorithm {
+			return fmt.Errorf("failed to get a signature algorithm with kty: %q, crv: %q, hash: %q", kty, crv, hash)
+		}
+
+		protectionLevel := getProtectionLevel(pl)
+		if protectionLevel == apiv1.UnspecifiedProtectionLevel {
+			return fmt.Errorf("unsupported protection level: %q", pl)
+		}
+
+		kuri, _ := cmd.Flags().GetString("kms")
+		if kuri == "" {
+			kuri = args[0]
+		}
+
+		cmd.SilenceUsage = true
+		km, err := kms.New(context.Background(), apiv1.Options{
+			Type: string(apiv1.PKCS11),
+			URI:  kuri,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to load key manager: %w", err)
+		}
+		defer km.Close()
+
+		resp, err := km.CreateKey(&apiv1.CreateKeyRequest{
+			Name:               args[0],
+			SignatureAlgorithm: signatureAlgorithm,
+			Bits:               size,
+			ProtectionLevel:    protectionLevel,
+			Extractable:        extractable,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to create key: %w", err)
+		}
+
+		block, err := pemutil.Serialize(resp.PublicKey)
+		if err != nil {
+			return fmt.Errorf("failed to serialize public key: %w", err)
+		}
+
+		if flagutil.MustBool(flags, "json") {
+			b, err := json.MarshalIndent(map[string]string{
+				"name":      resp.Name,
+				"publicKey": string(pem.EncodeToMemory(block)),
+			}, "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to marshal: %w", err)
+			}
+			fmt.Println(string(b))
+		} else {
+			fmt.Print(string(pem.EncodeToMemory(block)))
+		}
+		return nil
+	},
+}
+
+type rsaParams struct {
+	hash string
+	pss  bool
+}
+
+var rsaSignatureAlgorithmMapping = map[rsaParams]apiv1.SignatureAlgorithm{
+	{"", false}:       apiv1.SHA256WithRSA,
+	{"SHA256", false}: apiv1.SHA256WithRSA,
+	{"SHA384", false}: apiv1.SHA384WithRSA,
+	{"SHA512", false}: apiv1.SHA512WithRSA,
+	{"SHA256", true}:  apiv1.SHA256WithRSAPSS,
+	{"SHA384", true}:  apiv1.SHA384WithRSAPSS,
+	{"SHA512", true}:  apiv1.SHA512WithRSAPSS,
+}
+
+type ecParams struct {
+	crv string
+}
+
+var ecSignatureAlgorithmMapping = map[ecParams]apiv1.SignatureAlgorithm{
+	{""}:     apiv1.ECDSAWithSHA256,
+	{"P256"}: apiv1.ECDSAWithSHA256,
+	{"P384"}: apiv1.ECDSAWithSHA384,
+	{"P521"}: apiv1.ECDSAWithSHA512,
+}
+
+type okpParams struct {
+	crv string
+}
+
+var okpSignatureAlgorithmMapping = map[okpParams]apiv1.SignatureAlgorithm{
+	{""}:        apiv1.PureEd25519,
+	{"ED25519"}: apiv1.PureEd25519,
+}
+
+func getSignatureAlgorithm(kty, crv, hash string, pss bool) apiv1.SignatureAlgorithm {
+	switch strings.ToUpper(kty) {
+	case "EC":
+		return ecSignatureAlgorithmMapping[ecParams{crv}]
+	case "RSA":
+		return rsaSignatureAlgorithmMapping[rsaParams{hash, pss}]
+	case "OKP":
+		return okpSignatureAlgorithmMapping[okpParams{crv}]
+	default:
+		return ecSignatureAlgorithmMapping[ecParams{crv}]
+	}
+}
+
+func getProtectionLevel(pl string) apiv1.ProtectionLevel {
+	switch strings.ToUpper(pl) {
+	case "", "SOFTWARE":
+		return apiv1.Software
+	case "HSM", "HARDWARE":
+		return apiv1.HSM
+	default:
+		return apiv1.UnspecifiedProtectionLevel
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	flags := createCmd.Flags()
+	flags.SortFlags = false
+
+	kty := flagutil.UpperValue("kty", []string{"EC", "RSA", "OKP"}, "EC")
+	crv := flagutil.NormalizedValue("crv", []string{"P256", "P384", "P521", "Ed25519"}, "P256")
+	hash := flagutil.NormalizedValue("hash", []string{"SHA256", "SHA384", "SHA512"}, "SHA256")
+	protectionLevel := flagutil.UpperValue("protection-level", []string{"SOFTWARE", "HSM"}, "SOFTWARE")
+
+	flags.String("kms", "", "The `uri` with the kms configuration to use")
+	flags.Var(kty, "kty", "The key `type` to build the certificate upon.\nOptions are EC, RSA or OKP")
+	flags.Var(crv, "crv", "The elliptic `curve` to use for EC and OKP key types.\nOptions are P256, P384, P521 or Ed25519 on OKP")
+	flags.Int("size", 3072, "The key size for an RSA key")
+	flags.Var(hash, "hash", "The hashing `algorithm` used in the signature.\nOptions are SHA256, SHA384 or SHA512")
+	flags.Var(protectionLevel, "protection-level", "The protection `level` used on some Cloud KMSs.\nOptions are SOFTWARE or HSM")
+	flags.Bool("pss", false, "Use RSA-PSS signature scheme instead of PKCS #1")
+	flags.Bool("extractable", false, "Mark the new key as extractable")
+	flags.Bool("json", false, "Show the output using JSON")
+}
---
+++ internal/flagutil/flagutil.go
@@ -0,0 +1,109 @@
+package flagutil
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+// MustString returns the string value of a flag with the given name, it will
+// panic if the flag does not exists or is not the desired type.
+func MustString(flagSet *pflag.FlagSet, name string) string {
+	v, err := flagSet.GetString(name)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// MustInt returns the int value of a flag with the given name, it will panic if
+// the flag does not exists or is not the desired type.
+func MustInt(flagSet *pflag.FlagSet, name string) int {
+	v, err := flagSet.GetInt(name)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// MustBool returns the bool value of a flag with the given name, it will panic
+// if the flag does not exists or is not the desired type.
+func MustBool(flagSet *pflag.FlagSet, name string) bool {
+	v, err := flagSet.GetBool(name)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+type value struct {
+	Name      string
+	Allowed   []string
+	Value     string
+	Normalize func(string) string
+}
+
+// Value returns a pflag.Value interface with that will only accept values from
+// a given list.
+func Value(name string, allowed []string, defaultValue string) pflag.Value {
+	return &value{
+		Name:    name,
+		Allowed: allowed,
+		Value:   defaultValue,
+	}
+}
+
+// UpperValue returns a pflag.Value interface with that will only accept
+// values from a given list, but before checking the allowed list it will
+// normalized values to upper-case.
+func UpperValue(name string, allowed []string, defaultValue string) pflag.Value {
+	return &value{
+		Name:    name,
+		Allowed: allowed,
+		Value:   defaultValue,
+		Normalize: func(s string) string {
+			return strings.ToUpper(s)
+		},
+	}
+}
+
+// NormalizedValue returns a pflag.Value interface with that will only accept
+// values from a given list, but before checking the allowed list it will
+// normalized values to upper-case and remove any dash or hyphens.
+func NormalizedValue(name string, allowed []string, defaultValue string) pflag.Value {
+	return &value{
+		Name:    name,
+		Allowed: allowed,
+		Value:   defaultValue,
+		Normalize: func(s string) string {
+			s = strings.ReplaceAll(strings.ToUpper(s), "-", "") // Dash, hyphen-minus (-)
+			s = strings.ReplaceAll(s, "\u2010", "")             // Hyphen (‐)
+			s = strings.ReplaceAll(s, "\u2011", "")             // Non breaking hyphen (‑)
+			s = strings.ReplaceAll(s, "\u2012", "")             // Figure Dash (‒)
+			s = strings.ReplaceAll(s, "\u2013", "")             // En dash (–)
+			return strings.ReplaceAll(s, "\u2014", "")          // Em dash (—)
+		},
+	}
+}
+
+func (v *value) String() string {
+	return v.Value
+}
+
+func (v *value) Set(s string) error {
+	if v.Normalize != nil {
+		s = v.Normalize(s)
+	}
+	for _, a := range v.Allowed {
+		if a == s {
+			v.Value = a
+			return nil
+		}
+	}
+	return fmt.Errorf("value for flag --%s is not valid, options are %s", v.Name, strings.Join(v.Allowed, ","))
+}
+
+func (v *value) Type() string {
+	return "string"
+}
