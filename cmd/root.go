a2de23848c7d6f7e872bc8ee63fd476448329267
Mariano Cano <mariano.cano@gmail.com>
2022-10-20 18:13:55 -0700

    Fix linter error

--- cmd/root.go
+++ cmd/root.go
@@ -67,6 +67,6 @@ func init() {
 	pemutil.PromptPassword = func(s string) ([]byte, error) {
 		fmt.Fprint(os.Stderr, s+": ")
 		defer fmt.Fprintln(os.Stderr)
-		return term.ReadPassword(int(syscall.Stderr))
+		return term.ReadPassword(syscall.Stderr)
 	}
 }
