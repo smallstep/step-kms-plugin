c65403113fd5d3f4e814b40b56b0570aa9b305ca
Mariano Cano <mariano.cano@gmail.com>
2023-02-06 19:53:19 -0800

    Attempt to fix release

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# Add build dependencies
 apt update
-apt install --no-install-recommends -y libpcsclite-dev libpcsclite-dev:arm64
+apt install --no-install-recommends -y libpcsclite-dev libpcsclite-dev:arm64 
+apt install --no-install-recommends -y pkg-config pkg-config:arm64
+
+# Fix "dubious ownership in repository" error
 git config --global --add safe.directory /go/src/github.com/smallstep/step-kms-plugin
-git log -1
 exec /entrypoint.sh $@
