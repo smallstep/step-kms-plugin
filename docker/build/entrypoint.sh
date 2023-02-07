e215197413f58b672a49fc343e56ef569a196cd2
Mariano Cano <mariano.cano@gmail.com>
2023-02-06 19:39:58 -0800

    Attempt to fix release

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 apt update
 apt install --no-install-recommends -y libpcsclite-dev libpcsclite-dev:arm64
+git config --global --add safe.directory /go/src/github.com/smallstep/step-kms-plugin
 git log -1
 exec /entrypoint.sh $@
