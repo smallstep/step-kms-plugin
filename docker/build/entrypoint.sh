4a167ab5252900083569274d601ae64f57b2a905
Mariano Cano <mariano.cano@gmail.com>
2023-02-08 12:24:16 -0800

    Use goreleaser-cross Go 1.20

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt update
-apt install --no-install-recommends -y libpcsclite-dev libpcsclite-dev:arm64
+apt install --no-install-recommends -y pkg-config libpcsclite-dev libpcsclite-dev:arm64
 
 exec /entrypoint.sh $@
