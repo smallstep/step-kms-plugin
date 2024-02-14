c6341074f91f694dcc6851d9fd7f624a27d0d2e1
Mariano Cano <mariano.cano@gmail.com>
2024-02-14 12:58:10 -0800

    Add SBOMs for binaries and archives
    This commit adds the goreleaser configuration for creating SBOM files
    for binaries and archives. It also upgrades goreleaser-cross to v1.22.

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -3,6 +3,9 @@
 set -e
 
 apt update
-apt install --no-install-recommends -y pkg-config libpcsclite-dev libpcsclite-dev:arm64
+apt install --no-install-recommends -y curl pkg-config libpcsclite-dev libpcsclite-dev:arm64
+
+# Install syft
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
 
 exec /entrypoint.sh $@
