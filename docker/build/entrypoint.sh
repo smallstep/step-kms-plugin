f651d91ff2e00bfede6a7d134ed1e684f2fbe4f4
max furman <mx.furman@gmail.com>
2025-12-04 10:02:44 -0800

    wip

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -3,11 +3,9 @@
 set -ex
 
 # Fix for missing bullseye repos:
-echo 'deb http://archive.debian.org/debian bullseye main
-deb http://deb.debian.org/debian-security bullseye-security main
-deb http://archive.debian.org/debian bullseye-updates main' > /etc/apt/sources.list
-
-apt-get install debian-archive-keyring
+echo 'deb http://deb.debian.org/debian bullseye main
+deb http://deb.debian.org/debian bullseye-updates main
+deb http://security.debian.org/debian-security bullseye-security main' > /etc/apt/sources.list
 
 apt update
 apt install --no-install-recommends -y curl pkg-config libpcsclite-dev libpcsclite-dev:arm64
