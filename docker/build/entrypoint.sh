f947d6503b7900b01f5de47d4ad741ed22410bbd
Mariano Cano <mariano.cano@gmail.com>
2025-12-04 14:26:51 -0800

    Update entrypoint for Ubuntu 24.04 (#286)
    This commit removes the legacy Bullseye repositories and uses the default ones
    because the newer versions of goreleaser-cross use Ubuntu 24.04.
    
    It also fixes an error in the current images, which do not include the Windows
    ARM64 compiler.

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-# Fix for missing bullseye repos:
-echo 'deb http://archive.debian.org/debian bullseye main
-deb http://deb.debian.org/debian-security bullseye-security main
-deb http://archive.debian.org/debian bullseye-updates main' > /etc/apt/sources.list
-
 apt update
 apt install --no-install-recommends -y curl pkg-config libpcsclite-dev libpcsclite-dev:arm64
 
 # Install syft
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+# Install llvm-mingw compiler for arm64 if necessary
+if [ ! -e /llvm-mingw ]; then
+    cd /
+    curl -sSfL "https://github.com/mstorsjo/llvm-mingw/releases/download/20251021/llvm-mingw-20251021-ucrt-ubuntu-22.04-x86_64.tar.xz" | bsdtar -xf -
+    ln -snf $(pwd)/llvm-mingw-20251021-ucrt-ubuntu-22.04-x86_64 /llvm-mingw
+    cd -
+fi
 
 exec /entrypoint.sh $@
